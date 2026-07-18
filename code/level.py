7ad8f4119a4e57cec2ccc0b3f91412d111ef0e63
Renan <reluanaveiga@gmail.com>
2026-07-18 16:41:26 -0300

    Creating Players and Enemies Moving Players and Enimes

--- code/level.py
+++ code/level.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+import random
 import sys
 
 import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_HEIGHT, COLOR_WHITE
+from code.Const import WIN_HEIGHT, COLOR_WHITE, MENU_OPTION, EVENT_ENEMY, SPAWM_TIME
 from code.entityFactory import EntityFactory
 from code.entity import Entity
 
@@ -19,7 +19,11 @@ class Level:
         self.game_mode = game_mode  # modo de jogo
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
         self.timeout = 20000 #20 segundos
+        pygame.time.set_timer(EVENT_ENEMY, SPAWM_TIME)
 
     def run(self):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
@@ -36,6 +40,9 @@ class Level:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
 
             # printed text
             self.level_text(
