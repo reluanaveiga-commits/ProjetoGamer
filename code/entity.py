2a4004ee198244398a3bc0bd6d658c4c2c72cd54
Renan <reluanaveiga@gmail.com>
2026-07-18 15:29:38 -0300

    Fixing Imports in Livel.py flie

--- code/entity.py
+++ code/entity.py
@@ -9,8 +9,8 @@ class Entity(ABC):
 
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.suf = pygame.image.load('./asset/' + name + '.png')
-        self.rect = self.suf.get_rect(left=position[0], top=position[1])
+        self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
 
     @abstractmethod
--- code/level.py
+++ code/level.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pygame
+import sys
 
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_HEIGHT, COLOR_WHITE
 from code.entityFactory import EntityFactory
 from code.entity import Entity
 
@@ -14,11 +19,50 @@ class Level:
         self.game_mode = game_mode  # modo de jogo
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('level1Bg'))
+        self.timeout = 20000 #20 segundos
 
     def run(self):
+        pygame.mixer_music.load(f'./asset/{self.name}.mp3')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
+
             for ent in self.entity_list:
-                self.window.blit(source=ent.suf, dest=ent.rect)
+                self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # printed text
+            self.level_text(
+                text_size=14,
+                text=f"[{self.name} - timeout: {self.timeout / 1000: .1f}s]",
+                text_color=COLOR_WHITE,
+                text_pos=(10, 5)
+            )
+
+            self.level_text(
+                text_size=14,
+                text=f"fps: {clock.get_fps(): .0f}",
+                text_color=COLOR_WHITE,
+                text_pos=(10, WIN_HEIGHT - 35)
+            )
+
+            self.level_text(
+                text_size=14,
+                text=f"ENTIDADES: {len(self.entity_list)}",
+                text_color=COLOR_WHITE,
+                text_pos=(10, WIN_HEIGHT - 20)
+            )
+
             pygame.display.flip()
 
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont('Arial', text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
